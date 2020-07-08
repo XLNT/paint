c11b65ac2c7ec3fc79665f9f57f4220d275326d4
Matt Condon (shrugs) <mattgcondon@gmail.com>
2020-07-08 16:32:26 -0400

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-bruise">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          hello <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
---
+++ tailwind.config.js
@@ -0,0 +1,63 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: ['"DM Sans"', ...defaultTheme.fontFamily.sans],
+      serif: ['"DM Sans"', ...defaultTheme.fontFamily.sans],
+      mono: defaultTheme.fontFamily.mono,
+    },
+    gradients: (theme) => ({
+      empty: ['180deg', theme('colors.smudge'), theme('colors.transparent')],
+    }),
+    extend: {
+      colors: {
+        transparent: 'rgba(1, 1, 1, 0)',
+        gesso: '#FFFFFF',
+        drywall: '#F8F8F8',
+        smudge: '#E8E9F1',
+        concrete: '#9C9CA5',
+        bruise: '#010024',
+        tomato: '#FF3333',
+        blackout: '#000000',
+        highlight: '#0500FF',
+      },
+      padding: {
+        'safe-left': 'env(safe-area-inset-left)',
+        'safe-top': 'env(safe-area-inset-top)',
+        'safe-right': 'env(safe-area-inset-right)',
+        'safe-bottom': 'env(safe-area-inset-bottom)',
+      },
+      boxShadow: {
+        elevated:
+          '0px 0px 1px rgba(1, 0, 36, 0.04), 0px 2px 8px rgba(232, 233, 241, 0.12), 0px 2px 8px rgba(1, 0, 36, 0.04);',
+      },
+      fontSize: {
+        subtext: '0.64rem',
+        base: '1.00rem',
+        h4: '1.125rem',
+        h3: '1.563rem',
+        h2: '2.441rem',
+        h1: '3.815rem',
+      },
+      width: {
+        '80': '20rem',
+        '96': '24rem',
+      },
+      maxWidth: {
+        '48': '12rem',
+        '10xl': '120rem',
+      },
+    },
+  },
+  variants: {
+    textColor: ['responsive', 'hover', 'focus', 'group-hover'],
+    borderColor: ['responsive', 'hover', 'focus', 'group-hover'],
+    backgroundColor: ['responsive', 'hover', 'focus', 'group-hover'],
+    opacity: ['hover', 'focus', 'disabled'],
+    cursor: ['disabled'],
+  },
+  plugins: [require('tailwindcss-plugins/gradients')],
+  purge: ['./pages/**/*.tsx', './client/components/**/*.tsx'],
+};
