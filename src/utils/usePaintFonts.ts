a6c75b32179245dd0d672153eaa9473ae0b2c4ba
Matt Condon (shrugs) <mattgcondon@gmail.com>
2020-07-23 18:03:48 -0400

    fix: only load webfontloader client-side

--- src/utils/usePaintFonts.ts
+++ src/utils/usePaintFonts.ts
@@ -1,17 +1,18 @@
-import WebFont from 'webfontloader';
 import { useEffect } from 'react';
 
 export function usePaintFonts() {
   useEffect(() => {
-    WebFont.load({
-      classes: false,
-      events: false,
-      google: {
-        families: ['DM Sans'],
-      },
-      typekit: {
-        id: 'nah1hrx',
-      },
-    });
+    import('webfontloader').then((WebFont) =>
+      WebFont.load({
+        classes: false,
+        events: false,
+        google: {
+          families: ['DM Sans'],
+        },
+        typekit: {
+          id: 'nah1hrx',
+        },
+      }),
+    );
   }, []);
 }
