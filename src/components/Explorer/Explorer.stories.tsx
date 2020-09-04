e0df4f7257e7231b93f94c33396e9b516b00281a
Matt Condon (shrugs) <mattgcondon@gmail.com>
2020-09-04 17:33:22 -0400

    fix: refactor explorer into intelligent container component

--- src/components/Explorer/Explorer.stories.tsx
+++ src/components/Explorer/Explorer.stories.tsx
@@ -1,170 +1,101 @@
-import React, { useState } from 'react';
-import { Explorer } from './Explorer';
+import React from 'react';
+import { Explorer, buttonBorderForState, BorderState } from './Explorer';
 import { cn } from '../../utils/cn';
-import { InlineButtonGroup } from '../InlineButton/InlineButtonGroup';
 import { InlineButton } from '../InlineButton/InlineButton';
-import { Skeleton } from '../Skeleton/Skeleton';
 import { Text } from '../Text/Text';
-import { Button } from '../Button/Button';
-import { Select } from '../Select/Select';
 
 import { ReactComponent as BackIcon } from '../../assets/icon/back.svg';
 import { ReactComponent as SearchIcon } from '../../assets/icon/search.svg';
 import { ReactComponent as MenuIcon } from '../../assets/icon/menu.svg';
-import { ReactComponent as CloseIcon } from '../../assets/icon/close.svg';
-import { ReactComponent as ExpandIcon } from '../../assets/icon/down arrow.svg';
-import { ReactComponent as CollapseIcon } from '../../assets/icon/down arrow.svg';
-import { ReactComponent as CancelIcon } from '../../assets/icon/close.svg';
 
 export default {
   title: 'Explorer',
 };
 
-interface SelectItem {
-  value: string;
-}
-const ITEMS: SelectItem[] = new Array(100).fill(0).map((_, i) => ({
-  value: i.toString(),
-}));
-
-const backLink = <InlineButton elementType="a" href="https://google.com" icon={<BackIcon />} />;
-const menu = (
-  <div className={cn('flex flex-col justify-center items-center p-4 space-y-4')}>
-    <Button>About</Button>
-    <Button>Saved works</Button>
-    <Button>Switch account</Button>
-    <Button>Terms of Use</Button>
-  </div>
-);
-const SEARCH_RESULTS = [
-  { key: 'a', title: 'Resource title', type: 'Work' },
-  { key: 'b', title: 'Resource title', type: 'Work' },
-  { key: 'c', title: 'Resource title', type: 'Work' },
-  { key: 'd', title: 'Resource title', type: 'Work' },
-];
-
-export const Empty = () => (
+export const Simple = () => (
   <Explorer
     className={cn('w-64')}
-    searchIcon={<SearchIcon />}
-    menuIcon={<MenuIcon />}
-    closeIcon={<CloseIcon />}
+    start={<InlineButton className={buttonBorderForState(BorderState.None)} icon={<BackIcon />} />}
+    middle={<Text className={buttonBorderForState(BorderState.None)}>Hello There</Text>}
+    end={<InlineButton className={buttonBorderForState(BorderState.None)} icon={<MenuIcon />} />}
   />
 );
 
-export const EmptyWithActions = () => (
+export const Popovered = () => (
+  <div className={cn('flex flex-row space-x-4')}>
+    <Explorer
+      className={cn('w-64')}
+      start={
+        <InlineButton
+          className={buttonBorderForState(BorderState.Selected)}
+          icon={<SearchIcon />}
+        />
+      }
+      middle={<Text className={buttonBorderForState(BorderState.Unselected)}>Hello There</Text>}
+      end={
+        <InlineButton
+          className={buttonBorderForState(BorderState.Unselected)}
+          icon={<MenuIcon />}
+        />
+      }
+      topPopunder={<div className={cn('h-24')} />}
+    />
+    <Explorer
+      className={cn('w-64')}
+      start={
+        <InlineButton
+          className={buttonBorderForState(BorderState.Unselected)}
+          icon={<SearchIcon />}
+        />
+      }
+      middle={<Text className={buttonBorderForState(BorderState.Unselected)}>Hello There</Text>}
+      end={
+        <InlineButton className={buttonBorderForState(BorderState.Selected)} icon={<MenuIcon />} />
+      }
+      topPopunder={<div className={cn('h-24')} />}
+    />
+  </div>
+);
+
+export const WithActions = () => (
   <Explorer
     className={cn('w-64')}
-    content={<Skeleton className={cn('w-full h-6')} />}
-    menu={menu}
-    searchIcon={<SearchIcon />}
-    menuIcon={<MenuIcon />}
-    closeIcon={<CloseIcon />}
+    start={<InlineButton className={buttonBorderForState(BorderState.None)} icon={<BackIcon />} />}
+    middle={<Text className={buttonBorderForState(BorderState.None)}>Hello There</Text>}
+    end={<InlineButton className={buttonBorderForState(BorderState.None)} icon={<MenuIcon />} />}
   >
-    <InlineButtonGroup className={cn('w-64')}>
-      <InlineButton className={cn('flex-1')} isDisabled>
-        <Skeleton className={cn('w-full h-6')} />
-      </InlineButton>
-      <InlineButton className={cn('flex-1')} isDisabled>
-        <Skeleton className={cn('w-full h-6')} />
-      </InlineButton>
-      <InlineButton className={cn('flex-1')} isDisabled>
-        <Skeleton className={cn('w-full h-6')} />
-      </InlineButton>
-    </InlineButtonGroup>
+    <InlineButton className={buttonBorderForState(BorderState.None)}>Medium</InlineButton>
+    <InlineButton className={buttonBorderForState(BorderState.None)}>Geography</InlineButton>
   </Explorer>
 );
 
-export const EventExplorer = () => {
-  const [search, setSearch] = useState('');
-  const [medium, setMedium] = useState<SelectItem | null>(null);
-  const [geo, setGeo] = useState<SelectItem | null>(null);
-  return (
+export const WithActionsPopovered = () => (
+  <div className={cn('flex flex-row space-x-4')}>
     <Explorer
       className={cn('w-64')}
-      search={search}
-      setSearch={setSearch}
-      content="Ars Electronica + .art Domains Curated"
-      searching={search.length === 1 || search.length === 2}
-      placeholder={<Text className={cn('p-2 text-center')}>Placeholder text...</Text>}
-      loading={<Text className={cn('p-2 text-center')}>Loading state...</Text>}
-      notFound={<Text className={cn('p-2 text-center')}>Not found state...</Text>}
-      items={search.length > 1 && search.length < 5 ? SEARCH_RESULTS : undefined}
-      itemToKey={(item) => item?.key}
-      itemToString={(item) => item?.title ?? ''}
-      onSelectItem={(item) => item && alert(item?.title)}
-      renderResultItem={({ item, active }) => (
-        <a href="https://google.com">
-          <div
-            className={cn(
-              'flex flex-row justify-between items-center',
-              'p-2',
-              active && 'bg-smudge',
-              'hover:bg-smudge',
-            )}
-          >
-            <Text className={cn('flex-1 truncate')}>{item.title}</Text>
-            <Text>{item.type}</Text>
-          </div>
-        </a>
-      )}
-      menu={menu}
-      searchIcon={<SearchIcon />}
-      menuIcon={<MenuIcon />}
-      closeIcon={<CloseIcon />}
+      start={
+        <InlineButton className={buttonBorderForState(BorderState.None)} icon={<BackIcon />} />
+      }
+      middle={<Text className={buttonBorderForState(BorderState.None)}>Hello There</Text>}
+      end={<InlineButton className={buttonBorderForState(BorderState.None)} icon={<MenuIcon />} />}
+      bottomPopunder={<div className={cn('h-24')} />}
     >
-      <InlineButtonGroup className={cn('w-full')}>
-        <Select
-          className={cn('flex-1')}
-          items={ITEMS}
-          selectedItem={medium}
-          onSelectItem={setMedium}
-          renderItem={({ item }) => item.value}
-          itemToKey={(item) => item.value}
-          renderIcon={({ open, hasSelectedItem }) =>
-            hasSelectedItem ? <CancelIcon /> : open ? <ExpandIcon /> : <CollapseIcon />
-          }
-        >
-          Medium
-        </Select>
-        <Select
-          className={cn('flex-1')}
-          items={ITEMS}
-          selectedItem={geo}
-          onSelectItem={setGeo}
-          itemToKey={(item) => item.value}
-          renderItem={({ item }) => item.value}
-          renderIcon={({ open, hasSelectedItem }) =>
-            hasSelectedItem ? <CancelIcon /> : open ? <ExpandIcon /> : <CollapseIcon />
-          }
-        >
-          Geography
-        </Select>
-      </InlineButtonGroup>
+      <InlineButton className={buttonBorderForState(BorderState.Selected)}>Medium</InlineButton>
+      <InlineButton className={buttonBorderForState(BorderState.Unselected)}>
+        Geography
+      </InlineButton>
     </Explorer>
-  );
-};
-
-export const SpaceExplorer = () => (
-  <Explorer
-    className={cn('w-64')}
-    start={backLink}
-    content="Space name"
-    menu={menu}
-    searchIcon={<SearchIcon />}
-    menuIcon={<MenuIcon />}
-    closeIcon={<CloseIcon />}
-  />
-);
-
-export const WorkExplorer = () => (
-  <Explorer
-    className={cn('w-64')}
-    start={backLink}
-    content={<span className={cn('italic')}>Work title</span>}
-    menu={menu}
-    searchIcon={<SearchIcon />}
-    menuIcon={<MenuIcon />}
-    closeIcon={<CloseIcon />}
-  />
+    <Explorer
+      className={cn('w-64')}
+      start={
+        <InlineButton className={buttonBorderForState(BorderState.None)} icon={<BackIcon />} />
+      }
+      middle={<Text className={buttonBorderForState(BorderState.None)}>Hello There</Text>}
+      end={<InlineButton className={buttonBorderForState(BorderState.None)} icon={<MenuIcon />} />}
+      bottomPopunder={<div className={cn('h-24')} />}
+    >
+      <InlineButton className={buttonBorderForState(BorderState.Unselected)}>Medium</InlineButton>
+      <InlineButton className={buttonBorderForState(BorderState.Selected)}>Geography</InlineButton>
+    </Explorer>
+  </div>
 );
--- src/components/Explorer/Explorer.tsx
+++ src/components/Explorer/Explorer.tsx
@@ -1,233 +1,104 @@
-import React, { PropsWithChildren, ReactNode, useReducer, ReactElement, cloneElement } from 'react';
-import { mergeProps } from '@react-aria/utils';
-import { useCombobox } from 'downshift';
+import React, { ReactElement, cloneElement } from 'react';
 import { cn } from '../../utils/cn';
 import { WithClassName } from '../../utils/WithClassName';
-import { InlineButton } from '../InlineButton/InlineButton';
-import { Text } from '../Text/Text';
+import { mergeProps } from '@react-aria/utils';
 
-interface ExplorerProps<TItem> extends WithClassName, PropsWithChildren<{}> {
-  search?: string;
-  setSearch?: (search: string) => void;
-  searching?: boolean;
-  placeholder?: ReactNode;
-  loading?: ReactNode;
-  notFound?: ReactNode;
-  items?: TItem[];
-  renderResultItem?: (state: { item: TItem; active: boolean }) => ReactNode;
-  itemToKey?: (item: TItem) => string;
-  itemToString?: (item: TItem | null) => string;
-  onSelectItem?: (item: TItem | null | undefined) => void;
-
-  menu?: ReactElement;
-  start?: ReactElement;
-  end?: ReactElement;
-  content?: ReactNode;
-
-  searchIcon: ReactElement;
-  menuIcon: ReactElement;
-  closeIcon: ReactElement;
+interface ExplorerProps extends WithClassName {
+  start: ReactElement;
+  middle: ReactElement;
+  end: ReactElement;
+  top?: ReactElement;
+  topPopunder?: ReactElement;
+  bottom?: ReactElement;
+  bottomPopunder?: ReactElement;
+  children?: ReactElement[];
 }
 
-interface State {
-  isSearching: boolean;
-  isMenu: boolean;
+export enum BorderState {
+  None,
+  Selected,
+  Unselected,
 }
 
-type Action = { type: 'toggleSearching' } | { type: 'toggleMenu' } | { type: 'defocusSearch' };
-
-function reducer(state: State, action: Action) {
-  switch (action.type) {
-    case 'toggleSearching':
-      return { ...state, isSearching: !state.isSearching, isMenu: false };
-    case 'toggleMenu':
-      return { ...state, isSearching: false, isMenu: !state.isMenu };
-    case 'defocusSearch':
-      return { ...state, isSearching: state.isSearching ? false : state.isSearching };
-
-    default:
-      return state;
-  }
-}
-
-const INITIAL_STATE: State = { isSearching: false, isMenu: false };
-
-export function Explorer<TItem>({
-  children,
-  className,
-  search,
-  setSearch,
-  searching,
-  placeholder,
-  loading,
-  notFound,
-  items,
-  renderResultItem,
-  itemToKey,
-  itemToString,
-  onSelectItem,
-  //
-  content,
-  start,
-  menu,
-  //
-  searchIcon,
-  menuIcon,
-  closeIcon,
-}: ExplorerProps<TItem>) {
-  // searching can only happen at the top level, where there is no back button
-  const canSearch = !!setSearch;
-  const noItems = !items || items.length === 0;
-  const hasSearchText = search && search.length > 0;
-
-  const [{ isSearching, isMenu }, dispatch] = useReducer(reducer, INITIAL_STATE);
-  const isPopover = isSearching || isMenu;
-  const {
-    getMenuProps,
-    getInputProps,
-    getComboboxProps,
-    highlightedIndex,
-    getItemProps,
-    reset,
-  } = useCombobox({
-    id: 'explorer',
-    labelId: 'explorer-label',
-    inputId: 'explorer-input',
-    menuId: 'explorer-menu',
-    items: items ?? [],
-    inputValue: search,
-    itemToString,
-    onInputValueChange: ({ inputValue }) => setSearch?.(inputValue ?? ''),
-    onSelectedItemChange: ({ selectedItem }) => onSelectItem?.(selectedItem),
+export const buttonBorderForState = (state: BorderState) =>
+  cn('border border-transparent', {
+    'border-l-bruise border-t-bruise border-r-bruise': state === BorderState.Selected,
+    'border-b-bruise': state === BorderState.Unselected,
   });
 
-  const startButtonClassnames = cn(
-    'border border-transparent',
-    isMenu && 'border-b-bruise',
-    isSearching && 'border-t-bruise border-l-bruise border-r-bruise',
-  );
-  const middleElementClassnames = cn(
-    'flex-1 min-w-0',
-    'border-b',
-    isPopover ? 'border-bruise' : 'border-transparent',
-  );
-  const endButtonClassnames = cn(
-    'border border-transparent',
-    isSearching && 'border-b-bruise',
-    isMenu && 'border-t-bruise border-l-bruise border-r-bruise',
-  );
+const popunderStyle = cn(
+  'absolute top-full left-0 right-0 z-10',
+  'bg-gesso',
+  'border-l border-r border-b border-bruise',
+);
 
-  const renderStartItem = () => {
-    if (canSearch) {
-      return (
-        <InlineButton
-          className={startButtonClassnames}
-          icon={searchIcon}
-          onPress={() => dispatch({ type: 'toggleSearching' })}
-        />
-      );
-    }
-
-    return start && cloneElement(start as ReactElement, { className: startButtonClassnames });
-  };
-
-  const renderMiddleItem = () => {
-    const input = (
-      <input
-        {...getInputProps({}, { suppressRefError: true })}
-        className={cn(
-          middleElementClassnames,
-          !isSearching && 'hidden',
-          'p-2 leading-normal',
-          'overflow-hidden',
-          'bg-transparent placeholder-concrete',
-          'outline-none appearance-none',
-        )}
-        autoFocus
-      />
-    );
-
-    if (isSearching) {
-      return input;
-    }
-
-    if (canSearch) {
-      return (
-        <>
-          {input}
-          <InlineButton
-            onPress={() => dispatch({ type: 'toggleSearching' })}
-            className={cn(middleElementClassnames)}
-            isDisabled={!canSearch}
-          >
-            {content}
-          </InlineButton>
-        </>
-      );
-    }
-
-    return (
-      <>
-        {input}
-        <Text
-          as="span"
-          className={cn(middleElementClassnames, 'flex-1', 'p-2 truncate select-none')}
-        >
-          {content}
-        </Text>
-      </>
-    );
-  };
-
-  const renderEndItem = () => {
-    return (
-      <InlineButton
-        {...mergeProps(isSearching ? { onPress: reset } : {}, {
-          onPress: () =>
-            isSearching ? dispatch({ type: 'toggleSearching' }) : dispatch({ type: 'toggleMenu' }),
-        })}
-        className={endButtonClassnames}
-        icon={isMenu || isSearching ? closeIcon : menuIcon}
-      />
-    );
-  };
-
+export function Explorer({
+  className,
+  start,
+  middle,
+  end,
+  top,
+  topPopunder,
+  bottom,
+  bottomPopunder,
+  children,
+}: ExplorerProps) {
   return (
-    <div className={cn(className, 'bg-gesso', 'flex flex-col min-w-0')}>
-      <div
-        className={cn('relative', 'flex flex-row items-stretch min-w-0')}
-        {...getComboboxProps()}
-      >
-        {renderStartItem()}
-        {renderMiddleItem()}
-        {renderEndItem()}
-        <div
-          key="menu"
-          className={cn(
-            !isPopover && 'hidden',
-            'absolute top-full left-0 right-0 z-10',
-            'bg-gesso',
-            'border-l border-r border-b border-bruise',
-          )}
-        >
-          {isMenu && menu}
-          {/* apparently this ul needs to always be in the dom for a11y reasons? */}
-          <ul className={cn(!isSearching && 'hidden')} {...getMenuProps()}>
-            {isSearching && !searching && noItems && hasSearchText && notFound}
-            {isSearching && !searching && noItems && !hasSearchText && placeholder}
-            {isSearching &&
-              items &&
-              items.length > 0 &&
-              items.map((item, index) => (
-                <li key={itemToKey?.(item)} {...getItemProps({ item, index })}>
-                  {renderResultItem?.({ item, active: highlightedIndex === index })}
-                </li>
-              ))}
-            {isSearching && searching && loading}
-          </ul>
-        </div>
-      </div>
-      {children && <div className={cn('flex flex-row')}>{children}</div>}
+    <div className={cn(className, 'bg-gesso', 'flex flex-col min-w-0 min-h-0')}>
+      {cloneElement(
+        top ?? <div />,
+        mergeProps(top?.props, {
+          className: cn('relative', 'flex flex-row items-stretch min-w-0 min-h-0'),
+        }),
+        [
+          cloneElement(
+            start,
+            mergeProps(start.props, { key: 'start', className: cn('flex-none') }),
+          ),
+          cloneElement(
+            middle,
+            mergeProps(middle.props, { key: 'middle', className: cn('flex-1', 'p-2') }),
+          ),
+          cloneElement(end, mergeProps(end.props, { key: 'end', className: cn('flex-none') })),
+          topPopunder &&
+            cloneElement(
+              topPopunder,
+              mergeProps(topPopunder?.props, {
+                key: 'popunder',
+                className: popunderStyle,
+              }),
+            ),
+        ],
+      )}
+      {children &&
+        cloneElement(
+          bottom ?? <div />,
+          mergeProps(bottom?.props, {
+            className: cn(
+              'relative',
+              'flex flex-row items-stretch min-w-0 min-h-0',
+              'border-t border-smudge',
+            ),
+          }),
+          [
+            React.Children.map(children, (child: ReactElement, i) =>
+              cloneElement(
+                child as ReactElement,
+                mergeProps(child.props, {
+                  className: cn('flex-1', i > 0 && 'border-l border-l-smudge'),
+                }),
+              ),
+            ),
+            bottomPopunder &&
+              cloneElement(
+                bottomPopunder,
+                mergeProps(bottomPopunder?.props, {
+                  key: 'popunder',
+                  className: popunderStyle,
+                }),
+              ),
+          ],
+        )}
     </div>
   );
 }
